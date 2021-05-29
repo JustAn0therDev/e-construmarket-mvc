5c2c1154aea77587c0a6d6467a93aa61f35a63c7
Ruan Montelo <ruanmontelo@gmail.com>
2021-05-29 17:15:35 -0300

    Primeiro commit do projeto e_construmarket_mvc

---
+++ e_construmarket_mvc/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using e_construmarket_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace e_construmarket_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        // TODO: passar para o arquivo de config
+        private readonly string ApiUrlLocalhost = "https://localhost:44370";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet("/produto")]
+        public async Task<ActionResult> Produto()
+        {
+            using var client = new HttpClient();
+            var response = await client.GetAsync(string.Format("{0}{1}", ApiUrlLocalhost, "/produto"));
+            var jsonResult = await response.Content.ReadAsStringAsync();
+
+            return await Task.FromResult<ActionResult>(Ok(jsonResult));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ e_construmarket_mvc/wwwroot/js/site.js
@@ -0,0 +1,71 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        url: `${window.location.href}produto`,
+        cache: true,
+        async: true,
+        success: function (result) {
+            populaProdutoTable(JSON.parse(result));
+        },
+        error: function (err) {
+            console.error(err);
+        }
+    });
+
+
+    // TODO: a pesquisa deve encontrar um produto de cada marca e ordenar os produtos do menor ao maior preco
+    $("#input-nome-produto").keyup(function () {
+
+        function mostrarApenasMensagemDaTabela() {
+            linhasDaTabela.each(function() { $(this).hide(); });
+            linhasDaTabela.first().show();
+        }
+        
+        const search = $(this).val();
+        const linhasDaTabela = $("#table-produtos tbody tr");
+        let contagemDeLinhasExibidas = 0;
+        
+        if (!search || search.length < 3) {
+            mostrarApenasMensagemDaTabela();
+            return;
+        }
+
+        linhasDaTabela.each(function () {
+            const linhaAtual = $(this);
+            
+            if (linhaAtual.find("td:first").text().toUpperCase().includes(search.toUpperCase())) {
+                linhaAtual.show();
+                contagemDeLinhasExibidas++;
+            } else {
+                linhaAtual.hide();
+            }
+        });
+
+        if (contagemDeLinhasExibidas === 0) {
+            mostrarApenasMensagemDaTabela();
+        }
+    });
+});
+
+function populaProdutoTable(produtosArray) {
+
+    function elementoNaoEncontrado(el) {
+        return !el || el.length === 0;
+    }
+
+    const table = $("#table-produtos");
+
+    if (elementoNaoEncontrado(table)) {
+        return;
+    }
+
+    const tableBody = table.find("tbody");
+
+    if (elementoNaoEncontrado(tableBody)) {
+        return;
+    }
+
+    produtosArray.forEach(produto => {
+        const valorFormatadoEmReais = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(produto.preco);
+        tableBody.append(`<tr style="display:none"><td>${produto.nome}</td><td>${valorFormatadoEmReais}</td><td>${produto.marca.nome}</td><td>${produto.marca.codigoMarca}</td></tr>`);
+    });
+}
